9a93312b7a7d42107f230f7bd46226a4902162af
Gizem BICER <gizem.bicer@accenture.com>
2022-09-20 21:33:54 +0300

    gradingStudents

---
+++ GradingStudents/Program.cs
@@ -0,0 +1,26 @@
+﻿class Result
+{
+    public static List<int> gradingStudents(List<int> grades)
+    {
+        List<int> result = new List<int>();
+        foreach (int i in grades)
+        {
+            if (i >= 38 && i % 5 > 2)
+                result.Add(i + (5 - i % 5));
+            else
+                result.Add(i);
+        }
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Console.WriteLine(String.Join("\n", Result.gradingStudents(arr)));
+    }
+}
