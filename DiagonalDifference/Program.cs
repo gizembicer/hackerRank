85341a8d41bcba147bbdc0989a673167d0969ce1
Gizem BICER <gizem.bicer@accenture.com>
2022-09-22 21:19:54 +0300

    diagonalDifference flippingBits

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,34 @@
+﻿class Result
+{
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int numberOfRows = arr.Count;
+        int firstDiagonal = 0, secondDiagonal = 0;
+        for (int i = 0; i < numberOfRows; i++)
+        {
+            firstDiagonal += arr[i][i];
+            secondDiagonal += arr[i][numberOfRows - 1 - i];
+        }
+        return Math.Abs(firstDiagonal - secondDiagonal);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.diagonalDifference(arr);
+        Console.WriteLine(result);
+
+    }
+}
---
+++ FlippingBits/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+
+class Result
+{
+    public static long flippingBits(long n)
+    {
+        var binary = Convert.ToString(n, 2);
+        binary = binary.PadLeft(32, '0');
+        string transformedBinary = string.Empty;
+        foreach (var item in binary)
+        {
+            switch (item)
+            {
+                case '1':
+                    transformedBinary += '0';
+                    break;
+                case '0':
+                    transformedBinary += '1';
+                    break;
+            }
+        }
+        
+        return Convert.ToInt64(transformedBinary, 2);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+            long result = Result.flippingBits(n);
+
+            Console.WriteLine(result);
+        }
+    }
+}
